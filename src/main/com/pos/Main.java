bc988fab0e7625b62ea0072d18eb19f482e8663e
unknown <victorkhornkh@gmail.com>
2026-01-07 22:59:19 +0700

    Update Product Panel

--- src/main/com/pos/Main.java
+++ src/main/com/pos/Main.java
@@ -1,12 +1,9 @@
 package main.com.pos;
 
-import java.awt.EventQueue;
-import main.com.pos.test.TestFrame;
+import main.com.pos.thread.MainThread;
 
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            new TestFrame().setVisible(true);
-        });
+        new MainThread().start();
     }
 }
