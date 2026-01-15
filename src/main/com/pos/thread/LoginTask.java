63a54b6163e8673f25c61baa50ee45bb664a220f
unknown <victorkhornkh@gmail.com>
2026-01-15 16:17:33 +0700

    Finish Product Panel

--- src/main/com/pos/thread/LoginTask.java
+++ src/main/com/pos/thread/LoginTask.java
@@ -1,19 +1,18 @@
 package main.com.pos.thread;
 
 import java.awt.EventQueue;
-import main.com.pos.components.layout.Layout;
-import main.com.pos.model.User;
 import main.com.pos.util.Color;
+import main.com.pos.view.login.LoginFrame;
 
 public class LoginTask implements Runnable {
 
     @Override
     public void run() {
-        User user = new User(
-            "a20260001" ,"victor", "123456", "admin", "Khorn Victor", "victor@gmail.com", 1, "images/avatar/victor.png"
-        );
+        // User user = new User(
+        //     "a20260001" ,"victor", "123456", "admin", "Khorn Victor", "victor@gmail.com", 1, "images/avatar/victor.png"
+        // );
         long startTime = System.currentTimeMillis();
-        EventQueue.invokeLater(() -> {new Layout(user).setVisible(true); });
+        EventQueue.invokeLater(() -> {new LoginFrame().setVisible(true); });
         long endTime = System.currentTimeMillis();
         System.out.println(Color.GREEN + "🛬  Login task completed in " + (endTime - startTime) + " ms" + Color.RESET);
     }
